2461c3fad5ba05b6d5c0839bf9efa3980082c609
Leo Osvald <leo.osvald@gmail.com>
2013-08-02 23:05:15 -0700

    Fix Python indentation of closing brackets

--- init-local.el
+++ init-local.el
@@ -1,1 +1,10 @@
 (load "~/.emacs.d/init-auctex")
+
+(require 'python)
+
+; Python
+(add-hook 'python-mode-hook
+    (lambda ()
+        (setq indent-tabs-mode t)
+        (setq tab-width 4)
+        (setq python-indent 4)))
--- init.el
+++ init.el
@@ -56,3 +56,29 @@
       (cons
        '("\\.m$" . octave-mode)
        auto-mode-alist))
+
+;; Python
+
+(require 'python)
+
+; Python indentation
+(defadvice python-calculate-indentation (around outdent-closing-brackets)
+  "Handle lines beginning with a closing bracket and indent them so that
+  they line up with the line containing the corresponding opening bracket."
+(save-excursion
+  (beginning-of-line)
+  (let ((syntax (syntax-ppss)))
+    (if (and (not (eq 'string (syntax-ppss-context syntax)))
+             (python-continuation-line-p)
+             (cadr syntax)
+             (skip-syntax-forward "-")
+             (looking-at "\\s)"))
+        (progn
+          (forward-char 1)
+          (ignore-errors (backward-sexp))
+          (setq ad-return-value (current-indentation)))
+      ad-do-it))))
+
+(add-hook 'python-mode-hook
+    (lambda ()
+        (ad-activate 'python-calculate-indentation)))
