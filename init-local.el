7e3f52987b384c413b0f07b38aa4ffaf4f5b99f1
Leo Osvald <leo.osvald@gmail.com>
2014-08-10 17:19:19 -0400

    Use spaces instead of tabs in python-mode (as PEP-8 suggests).

--- init-local.el
+++ init-local.el
@@ -2,13 +2,6 @@
 
 (require 'python)
 
-; Python
-(add-hook 'python-mode-hook
-    (lambda ()
-        (setq indent-tabs-mode t)
-        (setq tab-width 4)
-        (setq python-indent 4)))
-
 ;; CScope
 (require 'xcscope)
 (cscope-setup)
--- init.el
+++ init.el
@@ -93,4 +93,7 @@
 
 (add-hook 'python-mode-hook
     (lambda ()
+        (setq indent-tabs-mode nil)
+        (setq tab-width 4)
+        (setq python-indent 4)
         (ad-activate 'python-calculate-indentation)))
