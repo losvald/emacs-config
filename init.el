9dfc20de3940c2e7dd017497a5991ecb69dfe90e
Leo Osvald <leo.osvald@gmail.com>
2014-09-24 17:03:56 -0400

    Load scratch init for .scratch.cpp files. Fix enum indent in C++11.

--- init.el
+++ init.el
@@ -55,6 +55,34 @@
 	    (my/bindkey-recompile)
 	    (c-set-offset 'inextern-lang 0)  ; don't indent "extern" blocks
 	    ))
+(add-to-list 'auto-mode-alist
+	     '(".scratch\\.cpp" . (lambda () (load "~/.emacs.d/scratch-cpp"))))
+
+;; This hack fixes indentation for C++11's "enum class" in Emacs.
+;; http://stackoverflow.com/questions/6497374/emacs-cc-mode-indentation-problem-with-c0x-enum-class/6550361#6550361
+
+(defun inside-class-enum-p (pos)
+  "Checks if POS is within the braces of a C++ \"enum class\"."
+  (ignore-errors
+    (save-excursion
+      (goto-char pos)
+      (up-list -1)
+      (backward-sexp 1) (looking-back "enum[ \t]+class[ \t]+[^}]+"))))
+
+(defun align-enum-class (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem)) 0
+    (c-lineup-topmost-intro-cont langelem)))
+
+(defun align-enum-class-closing-brace (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem)) '- '+))
+
+(defun fix-enum-class ()
+  "Setup `c++-mode' to better handle \"class enum\"."
+  (add-to-list 'c-offsets-alist '(topmost-intro-cont . align-enum-class))
+  (add-to-list 'c-offsets-alist
+	       '(statement-cont . align-enum-class-closing-brace)))
+
+;; (add-hook 'c++-mode-hook 'fix-enum-class)
 
 ;; Shell
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
@@ -108,3 +136,10 @@
 	      (kbd "C-c C-t") (lambda () (interactive) (sbt-command "test")))
 	    (define-key scala-mode-map
 	      (kbd "C-c C-r") (lambda () (interactive) (sbt-command "run")))))
+
+;; Valgrind
+;; (defun valgrind ()
+;;   (interactive)
+;;   (compilation-minor-mode)
+;;   (define-key compilation-minor-mode-map (kbd "") 'comint-send-input)
+;;   (define-key compilation-minor-mode-map (kbd "S-") 'compile-goto-error))
