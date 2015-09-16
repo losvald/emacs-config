d60bc53c537e81b36cfef06e2a1830b945851e23
Leo Osvald <leo.osvald@gmail.com>
2015-09-15 23:06:00 -0400

    Bind yas-reload-all to a convenient key (C-c Y)

--- init-yas.el
+++ init-yas.el
@@ -10,6 +10,7 @@
 
 ;; Yasnippet configuration
 (yas-global-mode 1)
+(define-key yas-minor-mode-map (kbd "C-c Y") 'yas-reload-all)
 (define-key yas-minor-mode-map [(tab)] nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (define-key yas-minor-mode-map (kbd "C-c C-n")
