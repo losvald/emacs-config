3ee9ae0600af742b9831178e1102797aad6d0269
Leo Osvald <leo.osvald@gmail.com>
2015-10-07 17:02:51 -0400

    Configure RE builder

--- init-ui.el
+++ init-ui.el
@@ -121,7 +121,32 @@
 ;;       (select-window orig-window))))
 ;; (ad-activate 'vc-annotate)
 
-;; Key bindings
+;; RE builder
+(require 're-builder)
+(setq reb-re-syntax 'string)
+(define-key reb-mode-map (kbd "C-c g")
+  (lambda () (interactive) (reb-change-syntax 'string)))
+(define-key reb-mode-map (kbd "C-c \\")
+  (lambda () (interactive) (reb-change-syntax 'read)))
+
+(defun reb-query-replace-this-regxp (replace)
+  "Uses the regexp built with re-builder to query the target buffer.
+This function must be run from within the re-builder buffer, not the target
+buffer.
+
+Argument REPLACE String used to replace the matched strings in the buffer.
+ Subexpression references can be used (\1, \2, etc)."
+  (interactive "sReplace with: ")
+  (if (eq major-mode 'reb-mode)
+      (let ((reg (reb-read-regexp)))
+        (select-window reb-target-window)
+        (save-excursion
+          (beginning-of-buffer)
+          (query-replace-regexp reg replace)))
+    (message "Not in a re-builder buffer!")))
+(define-key reb-mode-map "\C-c\M-%" 'reb-query-replace-this-regxp)
+
+;; Global key bindings
 (global-set-key (kbd "s-x") 'clipboard-kill-region)
 (global-set-key (kbd "s-c") 'clipboard-kill-ring-save)
 (global-set-key (kbd "s-v") 'x-clipboard-yank)
