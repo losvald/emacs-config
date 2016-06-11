60803b72ccfb95ec560be3ea1ca2db09affa4b7d
Leo Osvald <leo.osvald@gmail.com>
2016-06-10 17:18:57 -0700

    Revert to vanilla Emacs binding for C-w

--- init.el
+++ init.el
@@ -24,7 +24,6 @@
 (global-set-key (kbd "C-c ~") 'reload-dotemacs)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
-(global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
 (global-set-key (kbd "C-c C-j") 'join-line)
