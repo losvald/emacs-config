e93d264bf384e88f2b2d5dfa216ac78b91038e1e
Leo Osvald <leo.osvald@gmail.com>
2016-07-01 20:46:24 -0700

    Use IPython as shell if available

--- init.el
+++ init.el
@@ -143,6 +143,18 @@
 ;; Python
 
 (require 'python)
+(when (executable-find "ipython")
+  (setq python-shell-interpreter "ipython")
+  (add-hook 'python-mode-hook	 ; make python-shell work with IPython
+	    (lambda ()
+	      (setq
+	       python-shell-prompt-regexp "In \\[[0-9]+\\]: \\|>>> "
+	       python-shell-prompt-block-regexp "   \\.\\.\\.+:? "
+	       python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+	       python-shell-completion-setup-code
+	       "from IPython.core.completerlib import module_completion"
+	       python-shell-completion-string-code
+	       "';'.join(get_ipython().Completer.all_completions('%s'))\n"))))
 
 ; Python indentation
 (defadvice python-calculate-indentation (around outdent-closing-brackets)
